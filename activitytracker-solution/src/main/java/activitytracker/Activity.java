892de14ca721058533f61a1955f1642d9cb7b7a1
kovacseni <kovacseni@gmail.com>
2021-07-18 22:48:59 +0200

    JPA gyakorlati feladatok_Egyszerű mentés JPA-val

---
+++ activitytracker-solution/src/main/java/activitytracker/Activity.java
@@ -0,0 +1,86 @@
+package activitytracker;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "activities")
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "activity_id")
+    private Long id;
+
+    @Column(name = "activity_start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "activity_description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "activity_type")
+    private ActivityType type;
+
+    public Activity() {
+    }
+
+    public Activity(LocalDateTime startTime, String description, ActivityType type) {
+        this.startTime = startTime;
+        this.description = description;
+        this.type = type;
+    }
+
+    public Activity(Long id, LocalDateTime startTime, String description, ActivityType type) {
+        this.id = id;
+        this.startTime = startTime;
+        this.description = description;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ActivityType getType() {
+        return type;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setType(ActivityType type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Activity activity = (Activity) o;
+        return Objects.equals(id, activity.id) && Objects.equals(startTime, activity.startTime) && Objects.equals(description, activity.description) && type == activity.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, startTime, description, type);
+    }
+}
